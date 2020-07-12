5a32fbf52fc81c99c2cfcb68992cca2bfed41897
baoduy93 <duybaovv@gmail.com>
2020-07-12 10:05:49 +0700

    khoi tao project

---
+++ githubdemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githubdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
